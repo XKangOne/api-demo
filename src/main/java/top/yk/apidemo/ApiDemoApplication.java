0558be6f6516868bcb99e4a0c365c40728d3dfb1
杨康 <1910297447@qq.com>
2023-10-28 23:08:28 +0800

    Dockerfile和工作流

---
+++ src/main/java/top/yk/apidemo/ApiDemoApplication.java
@@ -0,0 +1,22 @@
+package top.yk.apidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class ApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDemoApplication.class, args);
+    }
+
+    @GetMapping("/list")
+    public List<String> getList(){
+        return List.of("吃饭", "睡觉","初代码");
+    }
+}
---
+++ src/test/java/top/yk/apidemo/ApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yk.apidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
